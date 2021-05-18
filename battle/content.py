04326807e869c25e3c58fcf8f2db25be5df09f84
mkdir-not-war <mkdir-not-war@users.noreply.github.com>
2021-05-18 13:13:29 -0500

    much less memory footprint, texture load

--- battle/content.py
+++ battle/content.py
@@ -6,66 +6,60 @@ texturedatafile = open('../data/resdata/texture-data.json',)
 TEX_DATA = json.load(texturedatafile)['textures']
 texturedatafile.close()
 
-class ContentLoader:
+class ContentManager:
 	def __init__(self):
 		self.content_name_dict = {}
 		self.num_loaded = 0
 		self.loaded = []	
 
-	def get_contentindex(self, contentid, indexinfile=0):
+	def get_contentindex(self, contentid):
 		result = -1
 		if (contentid in self.content_name_dict):
 			fileindex = self.content_name_dict[contentid]
-			result = fileindex + indexinfile
+			result = fileindex
 		else:
 			# load the content from the file
-			new_content, length = self.load(contentid)
+			new_content = self.load(contentid)
 			# add the texture/animation/etc to loaded
-			assert(length > 0)
-			assert(indexinfile < length)
-			result = self.num_loaded + indexinfile
-			self.loaded += new_content # append all items in new_content to loaded
-			self.num_loaded += length
+			self.loaded.append(new_content)
+			newcontentindex = self.num_loaded
+			self.num_loaded += 1
 			# set dict hash for quicker access later (consider counting usage for smart unloading)
-			self.content_name_dict[contentid] = new_content
+			self.content_name_dict[contentid] = newcontentindex
+			result = newcontentindex
 		return result
 
 	def load(self, contentid):
 		# TODO: override this in child classes
 		return ([], 0)
 
-class TextureLoader(ContentLoader):
+class TextureManager(ContentManager):
 	def load(self, textureid):
-		new_textures = []
-		length = 0
-
-		# get info from json
-		texturedata = TEX_DATA[textureid]
-
 		# load the full image file (pygame, sdl2, etc)
-		filepath = '../%s' % texturedata['filepath']
-		parenttexture = image_load(filepath)
+		filepath = '../%s' % TEX_DATA[textureid]['filepath']
+		new_texture = image_load(filepath)
 
-		# load each piece of the image file into separate textures, and increment length
-		current_offset_x, current_offset_y = 0, 0
+		return new_texture
+
+	''' get -> pygame.Surface '''
+	def get(self, textureid, framepos_x=0, framepos_y=0):
+		textureindex = self.get_contentindex(textureid)
+		assert(textureindex != -1) 
+
+		texturedata = TEX_DATA[textureid]
 		frame_dim = (texturedata['frame-width'], texturedata['frame-height'])
-
-		width, height = texturedata['width'], texturedata['height']
-		length += width * height
-
 		buffer_dim = texturedata['buffer-width'], texturedata['buffer-height']
+		frame_offset = (
+			framepos_x * (frame_dim[0] + buffer_dim[0]), 
+			framepos_y * (frame_dim[1] + buffer_dim[1])
+		)
+		texture_rect = Rect(frame_offset, frame_dim)
 
-		for j in range(height):
-			current_offset_y = j * frame_dim[1] + buffer_dim[1]
-			for i in range(width):
-				current_offset_x = i * frame_dim[0] + buffer_dim[0]
-				current_offset = (current_offset_x, current_offset_y)
-				text_rect = Rect(current_offset, frame_dim)
-				new_textures.append(parenttexture.subsurface(text_rect))
+		result = self.loaded[textureindex].subsurface(texture_rect)
 
-		return (new_textures, length)
+		return result
 
-class AnimationLoader(ContentLoader):
+class AnimationManager(ContentManager):
 	def load(self, animationid):
 		new_animations = []
 		length = 0
